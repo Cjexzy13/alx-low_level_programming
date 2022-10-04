d98d3662be7400b654cc804da9981cf721bcc939
Cjexzy13 <okojijohn@gmail.com>
2022-10-04 12:48:07 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and initializes it with a specific
- * @size: the size the main function gave
- * @c: the character we are given
- * Return: pointer else return null
- */
+* create_array - creates an array of chars, and initializes it with a specific
+* @size: the size the main function gave
+* @c: the character we are given
+* Return: pointer else return null
+*/
 
 char *create_array(unsigned int size, char c)
 {
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,1 +1,1 @@
-a function that returns a pointer to a newly allocated space in memory
+ a function that creates an array of chars, and initializes it with a specific char
